fed9579a77f80b41917159e99fa168b3c1c2c0e3
Michael Pearson <zl2mjp@gmail.com>
2016-05-03 17:32:21 +1200

    Added thread and stuff

--- app/src/main/java/nz/co/michaelpearon/robotcontrol/MainActivity.java
+++ app/src/main/java/nz/co/michaelpearon/robotcontrol/MainActivity.java
@@ -1,18 +1,56 @@
 package nz.co.michaelpearon.robotcontrol;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.net.Socket;
+import java.util.EventListener;
+
 public class MainActivity extends AppCompatActivity {
+    private JoystickView leftJoystick;
+    private JoystickView rightJoystick;
+
+    private int speed = 0;
+    private int port = 0;
+    private String ipAddress;
+
+    private TransmitterThread transmitterThread;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        leftJoystick = (JoystickView)findViewById(R.id.joystick_left);
+        rightJoystick = (JoystickView)findViewById(R.id.joystick_right);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        speed = sp.getInt("speed", 0);
+        port = sp.getInt("port", 0);
+        ipAddress = sp.getString("ipAddress", "");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        transmitterThread.stopTransmitting();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        leftJoystick = null;
+        rightJoystick = null;
     }
 
     @Override
@@ -28,4 +66,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
         return true;
     }
+
+
 }
---
+++ app/src/main/java/nz/co/michaelpearon/robotcontrol/TransmitterThread.java
@@ -0,0 +1,43 @@
+package nz.co.michaelpearon.robotcontrol;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TransmitterThread extends Thread {
+    private Socket socket;
+    private OutputStream outputStream;
+    private DataReference dataReference;
+    private static final byte[] data = new byte[6];
+    private int period;
+    private boolean stop = false;
+
+    public TransmitterThread(Socket socket, DataReference dataReference, int frequency) throws IOException {
+        this.dataReference = dataReference;
+        this.socket = socket;
+        this.outputStream = socket.getOutputStream();
+        this.period = 1 / frequency;
+    }
+
+    @Override
+    public void run() {
+        data[0] = dataReference.getX1();
+        data[1] = dataReference.getY1();
+        data[2] = dataReference.getX2();
+        data[3] = dataReference.getY2();
+        data[4] = dataReference.getSpeed();
+        data[5] = dataReference.getButtons();
+        while(socket.isConnected() && !isInterrupted() && !stop) {
+            try {
+                outputStream.write(data);
+                sleep(period);
+            } catch (IOException | InterruptedException e) {
+                return;
+            }
+        }
+    }
+
+    public void stopTransmitting() {
+        stop = true;
+    }
+}
